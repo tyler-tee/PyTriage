d989b7da9327af8287168bf35241001774a3c0ae
tyler-tee <64701075+tyler-tee@users.noreply.github.com>
2020-07-19 08:15:29 -0400

    +Setup, Reqs files

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+from os import path
+
+cur_directory = path.abspath(path.dirname(__file__))
+with open(path.join(cur_directory, 'README.md'), encoding='utf-8') as f:
+    long_description=f.read()
+
+setup(
+    name='PyTriage',
+    version='0.1.1',
+    packages=['pytriage'],
+    url='https://github.com/tyler-tee/pytriage',
+    license='GPLv3',
+    author='Tyler Talaga',
+    author_email='ttalaga@wgu.edu',
+    description='PyTriage is a Python library for interacting with Cofense Triage\'s v2 API.',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
